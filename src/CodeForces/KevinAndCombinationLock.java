03390d55fb42c4aa1f62a0234d14c3cc94b97714
pratham40 <122166099+pratham40@users.noreply.github.com>
2024-12-20 16:40:13 +0530

    HouseRobber.java

--- src/CodeForces/KevinAndCombinationLock.java
+++ src/CodeForces/KevinAndCombinationLock.java
@@ -8,23 +8,11 @@ public class KevinAndCombinationLock {
         int n = sc.nextInt();
         while (n-->0){
             int x = sc.nextInt();
-            while (x>=0){
-                String str = String.valueOf(x);
-                if (str.contains("33") && x!=33){
-                    str=str.replace("33","");
-                    x=Integer.parseInt(str);
-                }else {
-                    x-=33;
-                }
-
-                if (x==0){
-                    System.out.println("Yes");
-                    break;
-                }
+            if(x%33==0){
+                System.out.println("YES");
+            }else {
+                System.out.println("NO");
             }
-           if (x!=0){
-               System.out.println("No");
-           }
         }
     }
 }
--- src/DP/DP1/HouseRobber.java
+++ src/DP/DP1/HouseRobber.java
@@ -1,7 +1,42 @@
 package DP.DP1;
 
+import java.util.Arrays;
+
 public class HouseRobber {
     public static void main(String[] args) {
+        int[] house = {9,1,3,8};
+        System.out.println(rob1(house,0));
+        int[] dp = new int[house.length];
+        Arrays.fill(dp,Integer.MAX_VALUE);
+        System.out.println(rob2(house,0,dp));
+    }
 
+    private static int rob2(int[] house, int idx, int[] dp) {
+        if (idx==house.length-1){
+            return house[idx];
+        }
+        if (idx>house.length-1){
+            return 0;
+        }
+        if (dp[idx]!=Integer.MAX_VALUE){
+            return dp[idx];
+        }
+        int pick=house[idx]+rob2(house,idx+2,dp);
+        int no_pick = rob2(house,idx+1,dp);
+        dp[idx]=Math.max(pick,no_pick);
+        return dp[idx];
+    }
+
+    private static int rob1(int[] house, int idx) {
+        if (idx==house.length-1){
+            return house[idx];
+        }
+        if (idx>house.length-1){
+            return 0;
+        }
+        int pick = house[idx]+rob1(house,idx+2);
+        int not_pick=rob1(house,idx+1);
+        int ans = Math.max(pick,not_pick);
+        return ans;
     }
 }
